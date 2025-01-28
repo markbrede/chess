d9ca2900f2ac49ccbc54e196e3388b7bb39a5aaa
markbrede <markjacobbrede@gmail.com>
2025-01-27 20:00:25 -0700

    Structured rookMoves after king moves. Enhanced for loops to ensure the piece move to the correct areas

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -63,7 +63,7 @@ public class ChessPiece {
         if (this.type == PieceType.BISHOP) {
             moves.addAll(getBishopMoves(board, myPosition));
         }
-        if (this.type == PieceType.QUEEN) {
+        /** if (this.type == PieceType.QUEEN) {
             moves.addAll(getQueenMoves(board, myPosition));
         }
         if (this.type == PieceType.KNIGHT) {
@@ -71,16 +71,19 @@ public class ChessPiece {
         }
         if (this.type == PieceType.PAWN) {
             moves.addAll(getPawnMoves(board, myPosition));
-        }
+        }**/
 
         return moves;
     }
 
+
+
+    /**KING**/
     private ArrayList<ChessMove> getKingMoves(ChessBoard board, ChessPosition myPosition) {
 
         ArrayList<ChessMove> KingMoves = new ArrayList<>();
 
-        /*Any direction one time*/
+        //Any direction one time
         ChessPosition[] positions = {
                 new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()),    //up
                 new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()+1),  //up right
@@ -91,7 +94,7 @@ public class ChessPiece {
                 new ChessPosition(myPosition.getRow(), myPosition.getColumn()-1),    //left
                 new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()-1)   //up left
         };
-        //check positions
+        //check positions with what I learned about enhanced for loops
         for (ChessPosition newPosition : positions) {
             //ensure the move is within the 1 8 matrix
             if (newPosition.getRow() >= 1 && newPosition.getRow() <= 8 &&
@@ -110,6 +113,87 @@ public class ChessPiece {
         return KingMoves;
     }
 
+
+
+    /**ROOK    similar process to what I did for the KING**/
+    /**TA NOTE! I know we got these to work but come back to these and try to make the continuous pieces
+     * work with for loops. (Concise purposeful code)*/
+    private ArrayList<ChessMove> getRookMoves(ChessBoard board, ChessPosition myPosition) {
+        ArrayList<ChessMove> RookMoves = new ArrayList<>();
+        //moves to any valid potion front-back and side-side
+        //up 1-8 squares only
+        ChessPosition[] upPositions = {
+                new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()+2, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()+3, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()+4, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()+5, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()+6, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()+7, myPosition.getColumn())
+        };
+
+        //down 1-8
+        ChessPosition[] downPositions = {
+                new ChessPosition(myPosition.getRow()-1, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()-2, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()-3, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()-4, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()-5, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()-6, myPosition.getColumn()),
+                new ChessPosition(myPosition.getRow()-7, myPosition.getColumn())
+        };
+
+        //right
+        ChessPosition[] rightPositions = {
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()+1),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()+2),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()+3),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()+4),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()+5),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()+6),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()+7)
+        };
+
+        //left
+        ChessPosition[] leftPositions = {
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()-1),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()-2),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()-3),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()-4),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()-5),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()-6),
+                new ChessPosition(myPosition.getRow(), myPosition.getColumn()-7)
+        };
+
+        //find all rook positions on the 8x8 matrix aka the board
+        for (ChessPosition[] travel : new ChessPosition[][]{upPositions, downPositions, rightPositions, leftPositions}) {
+            for (ChessPosition newPosition : travel) {
+                if (newPosition.getRow() >= 1 && newPosition.getRow() <= 8 &&
+                        newPosition.getColumn() >= 1 && newPosition.getColumn() <= 8) {
+
+                    ChessPiece pieceOnPosition = board.getPiece(newPosition);
+                        //if the pieceOnPosition is null add position
+                    if (pieceOnPosition == null) {
+                        RookMoves.add(new ChessMove(myPosition, newPosition, null));
+                    } else {
+                            //if null is not found identify that the position has the other teams piece
+                        if (pieceOnPosition.getTeamColor() != this.getTeamColor()) {
+                            RookMoves.add(new ChessMove(myPosition, newPosition, null));
+                        }
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return RookMoves;
+    }
+
+
+
+    /**THE RECOMMENDED IntelliJ AUTOFILLED OVERRIDE STATEMENTS FOR toString, hashCode, AND EQUALS**/
     @Override
     public boolean equals(Object o) {
         if (this == o) {
