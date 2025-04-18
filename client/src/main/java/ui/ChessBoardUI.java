997f15d00124748983796db9f68cb4a43024aaf2
markbrede <markjacobbrede@gmail.com>
2025-04-17 19:33:46 -0600

    (Phase 5) Pseudo code notes for ChessBoardUI structure and requirements. Needed for PostloginUI

--- client/src/main/java/ui/ChessBoardUI.java
+++ client/src/main/java/ui/ChessBoardUI.java
@@ -1,4 +1,14 @@
 package ui;
 
+import chess.ChessPiece;
+import chess.ChessPosition;
+
 public class ChessBoardUI {
+    //needs to draw board/ board rows
+    //determine square color
+    //set the background color
+    //get a piece
+    //draw a piece
+    //should have column labels
+    //needs to draw the piece
 }
